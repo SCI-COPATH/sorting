8ecd55019331f2f641e5af36ea272128b8d7eb33
Jayasankar JP <scicopath01@gmail.com>
2021-02-16 01:42:09 +0000

    Add selection sort

--- sorting.c
+++ sorting.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 void swap(int *a,int *b){
-    *a=*a+*b;
-    *b=*a-*b;
-    *a=*a-*b;
+    int temp=*a;
+    *a=*b;
+    *b=temp;
 }
 void bubleSort(int ar[],int n){
     int i,j;
@@ -23,19 +23,34 @@ void insertionSort(int ar[],int n){
         ar[j+1]=current;
     }
 }
+void selectionSort(int ar[],int n){
+    int i,j,minIndex;
+    for(i=0;i<n-1;i++){
+        minIndex=i;
+        for(j=i+1;j<n;j++)
+            if(ar[j]<ar[minIndex])
+                minIndex=j;
+        swap(&ar[minIndex],&ar[i]);
+    }
+
+}
 void arrayPrint(int ar[],int n){
     int i;
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++)    
         printf("%d ",ar[i]);
     printf("\n");
 }
 void main(){
     int array1[]={10,5,8,1,55,4,36,33};
     int array2[]={20,5,45,1,2,55,6,78,12};
+    int array3[]={8,4,1,2,6,11,55,5,66};
     printf("Sorted Array 1 is ");
     bubleSort(array1,sizeof(array1)/sizeof(int));
     arrayPrint(array1,sizeof(array1)/sizeof(int));
     printf("Sorted Array 2 is ");
     insertionSort(array2,sizeof(array2)/sizeof(int));
     arrayPrint(array2,sizeof(array2)/sizeof(int));
+    printf("Sorted Array 3 is ");
+    selectionSort(array3,sizeof(array3)/sizeof(int));
+    arrayPrint(array3,sizeof(array3)/sizeof(int));
 }
